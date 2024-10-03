95411af9734db1f0749304f87657d8ab2d7428a8
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-10-03 10:39:14 +0200

    Aggiunto funzione SfondoCasuale

--- Es_SfondoCasuale.php
+++ Es_SfondoCasuale.php
@@ -7,6 +7,21 @@
 </head>
 <body>
     <?php
+        function sfondoCasuale($lar){
+            $c;
+            if ($lar < 150) {
+                $c = "#7FFFD4";
+            } elseif ($lar >= 150 && $lar <= 250) {
+                $c = "#1E90FF";
+            } else {
+                $c = "#0000FF";
+            }
+            return $c;
+        }
+
+        function createDiv($lar, $c){
+            echo "";
+        }
     ?>
 </body>
 </html>
